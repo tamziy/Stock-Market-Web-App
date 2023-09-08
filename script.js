2b6a13b8a6760e2b4b5973728d4d26269395e8c0
tamziy <114886626+tamziy@users.noreply.github.com>
2023-09-08 13:49:51 -0500

    Protecting API keys

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("JavaScript is working!");
+const apiKey = "df00cc47a1d4476584e22ccbe5310d31";
+const symbol = "AAPL";
+const url = `https://api.twelvedata.com/time_series?symbol=${symbol}&interval=1min&apikey=${apiKey}`;
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        const stockDataDiv = document.getElementById("stockData");
+        if (data.values && data.values.length > 0) {
+            const latestClosePrice = data.values[0].close;
+            stockDataDiv.innerHTML = `Latest closing price of ${symbol} is ${latestClosePrice}`;
+        } else {
+            stockDataDiv.innerHTML = "No data available";
+        }
+    })
+    .catch(error => console.log(error));
