ed951b2de105ea9af44cfdd77588f2c217ef6f25
tamziy <114886626+tamziy@users.noreply.github.com>
2023-10-02 00:32:28 -0500

    Added chart

--- script.js
+++ script.js
@@ -21,6 +21,38 @@ function fetchStockData(symbol) {
                 document.getElementById("highPrice").innerHTML = `High Price: ${latestData.high}`;
                 document.getElementById("lowPrice").innerHTML = `Low Price: ${latestData.low}`;
                 document.getElementById("volume").innerHTML = `Volume: ${latestData.volume}`;
+
+                // Prepare data for the chart
+                const labels = data.values.map(item => item.datetime).reverse();
+                const prices = data.values.map(item => item.close).reverse();
+
+                // Create the chart
+                const ctx = document.getElementById('stockChart').getContext('2d');
+                const stockChart = new Chart(ctx, {
+                    type: 'line',
+                    data: {
+                        labels: labels,
+                        datasets: [{
+                            label: `${symbol} Closing Prices`,
+                            data: prices,
+                            borderColor: 'rgba(75, 192, 192, 1)',
+                            fill: false
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            x: {
+                                type: 'time',
+                                time: {
+                                    unit: 'day'
+                                }
+                            },
+                            y: {
+                                beginAtZero: false
+                            }
+                        }
+                    }
+                });
             } else {
                 document.getElementById("stockData").innerHTML = "No data available";
             }
@@ -31,6 +63,7 @@ function fetchStockData(symbol) {
             console.log(error);
         });
 }
+
 
 
 
